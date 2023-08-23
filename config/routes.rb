934e2761fe3f0b9f928935f3e5474daf0eb71557
cgjg0267 <130421545+cgjg0267@users.noreply.github.com>
2023-08-23 11:54:12 +0000

    Commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  
+  root to: "homes#top"
+  get '/home/about' => 'homes#about', as: 'home_about'
+  
+  devise_for :users
+  
+  resources :books, only: [:show, :edit, :index, :update, :destroy, :create]
+  resources :users, only: [:show, :edit, :index, :update]
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
