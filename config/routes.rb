f23ef9a0df4c386b88589647f0d91050762ada12
cgjg0267 <130421545+cgjg0267@users.noreply.github.com>
2023-08-24 07:57:03 +0000

    Commit2

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :books, only: [:show, :edit, :index, :update, :destroy, :create]
   resources :users, only: [:show, :edit, :index, :update]
   
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
